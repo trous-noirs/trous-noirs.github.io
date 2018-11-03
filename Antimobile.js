fc3893479834dc505f28e99adc23d4a5d2d5a225
trous-noirs <44733886+trous-noirs@users.noreply.github.com>
2018-11-03 18:31:27 -0400

    Update Antimobile.js

--- Antimobile.js
+++ Antimobile.js
@@ -1,3 +1,3 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        display (</p1> Veuillez réessayer sur un ordinateur..<p1>)
+        display (<p> Veuillez réessayer sur un ordinateur..</p>)
 }
